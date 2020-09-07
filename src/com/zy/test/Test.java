ce6099b4f5355f0bcb24aea613d20b328fa3567d
zhouyong22 <33778679+zhouyong22@users.noreply.github.com>
2020-09-07 17:43:01 +0800

    Update Test.java

--- src/com/zy/test/Test.java
+++ src/com/zy/test/Test.java
@@ -2,9 +2,10 @@ package com.zy.test;
 
 public class Test {
     public static void main(String[] args) {
-    	System.out.println("²âÊÔ");
+    	System.out.println("Â²Ã¢ÃŠÃ”");
     	System.out.println("zhouyong");
     	System.out.println("zhouyong2");
+        System.out.println("zhouyong3333333");
 		
 	}
 }
